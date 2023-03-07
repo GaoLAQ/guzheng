b8f5f7d4fb67bd8b18418325bb8348c462263b95
haoxiang <gaolaq2019@gmail.com>
2023-03-07 22:53:49 +0000

    remove dynamic router, fix static router

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,55 +14,87 @@ import Opportunity from "../views/Opportunity.vue";
 // @ts-ignore
 import Message from "../views/Message.vue";
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    alias: "/home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: About,
+  },
+  {
+    path: "/lessons",
+    name: "lessons",
+    component: Lessons,
+  },
+  {
+    path: "/teachers",
+    name: "teachers",
+    component: Teachers,
+  },
+  {
+    path: "/shop",
+    name: "shop",
+    component: Shop,
+  },
+  {
+    path: "/opportunity",
+    name: "opportunity",
+    component: Opportunity,
+  },
+  {
+    path: "/message",
+    name: "message",
+    component: Message,
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      alias: "/home",
-      component: HomeView,
-    },
-  ],
+  routes,
 });
 
-router.addRoute({ path: "/about", component: About });
-router.addRoute({
-  path: "/lessons",
-  name: "lessons",
-  component: Lessons,
-});
+// router.addRoute({ path: "/about", name: "about", component: About });
+// router.addRoute({
+//   path: "/lessons",
+//   name: "lessons",
+//   component: Lessons,
+// });
 
-router.addRoute({
-  path: "/teachers",
-  name: "teachers",
-  component: Teachers,
-});
-router.addRoute({
-  path: "/shop",
-  name: "shop",
-  // route level code-splitting
-  // this generates a separate chunk (About.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: Shop,
-});
+// router.addRoute({
+//   path: "/teachers",
+//   name: "teachers",
+//   component: Teachers,
+// });
+// router.addRoute({
+//   path: "/shop",
+//   name: "shop",
+//   // route level code-splitting
+//   // this generates a separate chunk (About.[hash].js) for this route
+//   // which is lazy-loaded when the route is visited.
+//   component: Shop,
+// });
 
-router.addRoute({
-  path: "/opportunity",
-  name: "opportunity",
-  // route level code-splitting
-  // this generates a separate chunk (About.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: Opportunity,
-});
+// router.addRoute({
+//   path: "/opportunity",
+//   name: "opportunity",
+//   // route level code-splitting
+//   // this generates a separate chunk (About.[hash].js) for this route
+//   // which is lazy-loaded when the route is visited.
+//   component: Opportunity,
+// });
 
-router.addRoute({
-  path: "/message",
-  name: "message",
-  // route level code-splitting
-  // this generates a separate chunk (About.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: Message,
-});
+// router.addRoute({
+//   path: "/message",
+//   name: "message",
+//   // route level code-splitting
+//   // this generates a separate chunk (About.[hash].js) for this route
+//   // which is lazy-loaded when the route is visited.
+//   component: Message,
+// });
 
 export default router;
